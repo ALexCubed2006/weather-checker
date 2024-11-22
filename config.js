6911e8059e4ce30965d544d2dd71532308ed6cd3
ALexCubed2006 <alexyakubitski@gmail.com>
2024-11-22 16:12:44 +0300

    Update config.js
    api http req changed to https req

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 export const WEATHER_API_KEY = '15a662508ba94518964174204241511'
 
 // api url
-export const WEATHER_API_URL = 'http://api.weatherapi.com/v1/'
-export const FORECAST_API_URL = 'http://api.weatherapi.com/v1/forecast.json'
-export const HISTORY_API_URL = 'http://api.weatherapi.com/v1/history.json'
-export const CURRENT_API_URL = 'http://api.weatherapi.com/v1/current.json'
-export const ASTRO_API_URL = 'http://api.weatherapi.com/v1/astronomy.json'
+export const WEATHER_API_URL = 'https://api.weatherapi.com/v1/'
+export const FORECAST_API_URL = 'https://api.weatherapi.com/v1/forecast.json'
+export const HISTORY_API_URL = 'https://api.weatherapi.com/v1/history.json'
+export const CURRENT_API_URL = 'https://api.weatherapi.com/v1/current.json'
+export const ASTRO_API_URL = 'https://api.weatherapi.com/v1/astronomy.json'
 
 // constants
 export const LANGUAGES = { ru: 'Русский', en: 'English' }
