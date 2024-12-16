7517f7aaecc2f30b476983ff906368e447493bc9
ALexCubed2006 <alexyakubitski@gmail.com>
2024-12-16 14:56:06 +0300

    forecast card

--- src/components/forecastCard/ForecastCard.jsx
+++ src/components/forecastCard/ForecastCard.jsx
@@ -1,8 +1,39 @@
 import React, { memo } from 'react'
 
 const ForecastCard = memo(({ forecast, day }) => {
-	console.log('[ForecastCard]', day)
-	return <div>ForecastCard</div>
+	console.log('[ForecastCard]', forecast)
+	return (
+		<div>
+			<div>{day}</div>
+			<div>
+				<div>
+					<div>
+						Max temp:
+						{forecast.day.maxtemp_c}
+					</div>
+
+					<div>
+						Min temp:
+						{forecast.day.mintemp_c}
+					</div>
+					<div>
+						Avg temp:
+						{forecast.day.avgtemp_c}
+					</div>
+				</div>
+				<img src={forecast.day.condition.icon} />
+
+				<div>
+					Humidity
+					{forecast.day.avghumidity}%
+				</div>
+				<div>
+					Precipitation
+					{forecast.day.totalprecip_mm}
+				</div>
+			</div>
+		</div>
+	)
 })
 
 export default ForecastCard
