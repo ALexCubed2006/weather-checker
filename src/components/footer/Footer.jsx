cd89fb836700bae6004809aaf0712e80992c9d68
Karina <myemail@gmail.com>
2025-01-15 09:00:22 +0300

    футер изменет

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,17 @@
 import { memo } from 'react'
 import { ROUTES } from '../../../config'
 import AppLink from '../../shared/AppLink/AppLink'
+import styles from './Footer.module.css'
 
 const Footer = memo(() => {
 	// TODO:TEAM расположите footer в нижнюю часть страницы
 	// TODO: сделать footer
 	return (
-		<div>
+		<div className={styles.footer}>
 			<div>
-				<AppLink path={ROUTES.main}>home</AppLink>
-				<AppLink path={ROUTES.story}>story</AppLink>
-				<AppLink path={ROUTES.sity}>sity</AppLink>
+				<AppLink path={ROUTES.main}>Домой</AppLink>
+				<AppLink path={ROUTES.story}>Архив</AppLink>
+				<AppLink path={ROUTES.sity}>Город</AppLink>
 			</div>
 			
 		</div>
